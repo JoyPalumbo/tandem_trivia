4c620c824bd656e166c8cf81f5def383f32bb770
Joy <greeniejoy@gmail.com>
2020-10-27 22:22:13 -0500

    added Question component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 // import logo from './logo.svg';
 import "./App.css";
+// import UserInfo from "./UserInfo";
+import Questions from "./Questions";
 
 function App() {
   return (
     <div className="App">
-      <h1>Trivia App</h1>
+      <h1>Welcome to Tandem's Trivia App!</h1>
+      {/* <UserInfo /> */}
+      <Questions />
     </div>
   );
 }
