bbc2434b9fe871b6232d6ed3689d5ea28c3abed2
Joy <greeniejoy@gmail.com>
2020-10-28 15:45:33 -0500

    have answers shuffled at random

--- src/Questions.jsx
+++ src/Questions.jsx
@@ -1,8 +1,10 @@
 import trivia from "./triviaData.json";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Questions() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  // const [allAnswers, setAnswers] = useState([]);
+  const [mixedAnswers, setMixedAnswers] = useState([]);
 
   const clickNextQuestion = () => {
     if (currentQuestion < 20) {
@@ -12,33 +14,76 @@ function Questions() {
     }
   };
 
+  const mixAnswers = () => {
+    const mixed = [
+      trivia[currentQuestion].correct,
+      ...trivia[currentQuestion].incorrect,
+    ].sort(() => Math.random() - 0.5);
+    console.log("mixed answers", mixed);
+    setMixedAnswers(mixed);
+  };
+
+  useEffect(() => {
+    // mixAnswers();
+  });
+
   return (
     <div className="Questions">
       <h1>Questions</h1>
       <div>
         <h1>{trivia[currentQuestion].question}</h1>
-        <button onClick={clickNextQuestion}>
-          {trivia[currentQuestion].correct}
-        </button>
-        {trivia[currentQuestion].incorrect.map((answer) => {
+        {!mixedAnswers.length ? (
+          <div>
+            <button
+              onClick={() => {
+                clickNextQuestion();
+              }}
+            >
+              {trivia[currentQuestion].correct}
+            </button>
+
+            {trivia[currentQuestion].incorrect.map((answer) => {
+              return (
+                <div>
+                  <button
+                    onClick={() => {
+                      clickNextQuestion();
+                    }}
+                  >
+                    {answer}
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          mixedAnswers.map((answer) => {
+            return (
+              <div>
+                <button
+                  onClick={() => {
+                    mixAnswers();
+                    clickNextQuestion();
+                  }}
+                >
+                  {answer}
+                </button>
+              </div>
+            );
+          })
+        )}
+
+        {/* {mixedAnswers.map((answer) => {
           return (
             <div>
-              <button onClick={clickNextQuestion}>{answer}</button>;
-            </div>
-          );
-        })}
-        {/* {trivia.map((question) => {
-          return (
-            <div>
-              <h1>{question.question}</h1>
-              <h3>{question.correct}</h3>
-              {question.incorrect.map((answer) => {
-                return (
-                  <div>
-                    <h3>{answer}</h3>
-                  </div>
-                );
-              })}
+              <button
+                onClick={() => {
+                  mixAnswers();
+                  clickNextQuestion();
+                }}
+              >
+                {answer}
+              </button>
             </div>
           );
         })} */}
