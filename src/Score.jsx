3022c3473171f833daa499df5a598fc7ea1fe0da
Joy <greeniejoy@gmail.com>
2020-10-28 01:09:03 -0500

    changed Score component to hooks

--- src/Score.jsx
+++ src/Score.jsx
@@ -1,30 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
-class Score extends React.Component {
-  constructor() {
-    super();
+function Score() {
+  const [score, setScore] = useState(0);
 
-    this.state = {
-      score: 0,
-    };
+  const updateScore = () => {
+    const newScore = score + 1;
+    setScore(newScore);
+    console.log("clicking to next question", newScore);
+  };
 
-    this.updateScore = this.updateScore.bind(this);
-  }
-
-  updateScore() {
-    this.setState({
-      score: this.state.score + 1,
-    });
-    console.log("we're clicking", this.state.score);
-  }
-  render() {
-    return (
-      <div className="Score">
-        <h1>Name's Score: {this.state.score}</h1>
-        <button onClick={this.updateScore}>click</button>
-      </div>
-    );
-  }
+  // render() {
+  return (
+    <div className="Score">
+      <h1>Name's Score: {score}</h1>
+      <button onClick={updateScore}>click</button>
+    </div>
+  );
 }
 
 export default Score;
