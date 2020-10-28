756223ec089c6b21e2d267c69fe043a166e97111
Joy <greeniejoy@gmail.com>
2020-10-28 00:57:03 -0500

    added state to count score

--- src/Score.jsx
+++ src/Score.jsx
@@ -4,12 +4,24 @@ class Score extends React.Component {
   constructor() {
     super();
 
-    this.state = { score: 0 };
+    this.state = {
+      score: 0,
+    };
+
+    this.updateScore = this.updateScore.bind(this);
+  }
+
+  updateScore() {
+    this.setState({
+      score: this.state.score + 1,
+    });
+    console.log("we're clicking", this.state.score);
   }
   render() {
     return (
       <div className="Score">
         <h1>Name's Score: {this.state.score}</h1>
+        <button onClick={this.updateScore}>click</button>
       </div>
     );
   }
