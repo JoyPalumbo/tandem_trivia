ab93a684ebf53349c5b7883a878335ec7ad70482
Joy <greeniejoy@gmail.com>
2020-10-27 22:22:35 -0500

    render question data on page

--- src/Questions.jsx
+++ src/Questions.jsx
@@ -1,7 +1,18 @@
+import trivia from "./triviaData.json";
+
 function Questions() {
   return (
     <div className="Questions">
       <h1>Questions</h1>
+      <div>
+        {trivia.map((question) => {
+          return (
+            <div>
+              <h1>{question.question}</h1>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
