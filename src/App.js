6bbbe373cb30ff4211249d564eeeedaf28dae2f8
Joy <greeniejoy@gmail.com>
2020-10-28 20:51:00 -0500

    passed data to children and vise versa

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 // import logo from './logo.svg';
+import React, { useState } from "react";
 import "./App.css";
-// import UserInfo from "./UserInfo";
+import UserInfo from "./UserInfo";
 import Questions from "./Questions";
+import Score from "./Score";
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [name, setName] = useState("");
+  const getNameFromChild = (nameFromChild) => {
+    console.log("this is our data from child", nameFromChild);
+    setName(nameFromChild);
+  };
+
+  const getScoreFromScore = (scoreFromChild) => {
+    console.log("this is our data from child", scoreFromChild);
+    setScore(scoreFromChild);
+  };
+
+  const getScoreFromQuestions = (scoreFromChild) => {
+    console.log("this is our data from child", scoreFromChild);
+    setScore(scoreFromChild);
+  };
+
   return (
     <div className="App">
       <h1>Welcome to Tandem's Trivia App!</h1>
-      {/* <UserInfo /> */}
-      <Questions />
+      <UserInfo callbackFromParent={getNameFromChild} />
+      <Score callbackFromParent={getScoreFromScore} name={name} />
+      <Questions callbackFromParent={getScoreFromQuestions} score={score} />
     </div>
   );
 }
